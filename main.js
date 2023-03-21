1e83d05c1b79408d9e3a8238485a8d27e91ea8eb
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-03-21 15:42:29 +0300

    initialize node + add webpack

---
+++ main.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/modules/weather.js":
+/*!********************************!*\
+  !*** ./src/modules/weather.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ getCityWeather)\n/* harmony export */ });\nasync function getWeatherData(city) {\n    try { //make API call and return response Object\n        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3cdd9a839ee1a7573b2f830649b4f7af&units=metric`);\n        if (!response.ok) throw new Error(`No city named ${city}.`);\n\n        const responseObject = await response.json();\n        return responseObject;\n    } catch (error) {\n        alert(error.message);\n        return null;\n    }\n}\n\nasync function processData(dataObj) {\n    if (dataObj == null) return; //prevent unrecognised locations\n\n    let location = dataObj.name + \", \" + dataObj.sys.country;\n    let mainWeather = dataObj.weather[0].main;\n    let weatherDesc = dataObj.weather[0].description;\n    let cityTemp = dataObj.main.temp;\n    let tempFeel = dataObj.main.feels_like;\n    let windSpeed = dataObj.wind.speed;\n    let humidity = dataObj.main.humidity;\n\n    return {location, mainWeather, weatherDesc, cityTemp, tempFeel, windSpeed, humidity};\n}\n\nasync function getCityWeather(city) {\n    let FullWeatherObj = await getWeatherData(city);\n    let filteredWeatherObj = processData(FullWeatherObj);\n    \n    return filteredWeatherObj;\n}\n\n\n//# sourceURL=webpack://weatherapp/./src/modules/weather.js?");
+
+/***/ }),
+
+/***/ "./src/script.js":
+/*!***********************!*\
+  !*** ./src/script.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_weather__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/weather */ \"./src/modules/weather.js\");\n\n\nconst searchBar = document.getElementById('searchTarget');\nconst searchForm = document.getElementById('searchForm');\n\nsearchForm.addEventListener('submit', (event) => {\n    event.preventDefault();\n    console.log(searchBar.value);\n\n    searchForm.reset();\n})\n\n\n//# sourceURL=webpack://weatherapp/./src/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/script.js");
+/******/ 	
+/******/ })()
+;
